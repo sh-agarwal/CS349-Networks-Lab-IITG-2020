819a7c7fcb48966ed3319ecbfdd3bc4510ae2bf2
sh-agarwal <shagarwal@yahoo.com>
2020-03-13 21:03:00 +0530

    Final Code

--- Assignment-3/client.c
+++ Assignment-3/client.c
@@ -12,6 +12,13 @@ int PORT=0;
 
 int main(int argc, char const *argv[]) 
 { 
+	//Checking Command line argument
+	if(argc<3)
+	{
+		perror("IP/PORT MISSING\n");
+		return 0;
+	}
+
 	for(int i=0;argv[2][i]!='\0';i++)
 	{
 		if(!isdigit(argv[2][i]))	//Invalid PORT
@@ -55,11 +62,11 @@ int main(int argc, char const *argv[])
 		printf("Asking UDP PORT\n");
 		send(sock , ask_udp_port , strlen(ask_udp_port) , 0 ); 
 		valread = read( sock , buffer, 1024); 
-		printf("Message Recieved: %s\n",buffer );
+		printf("Message Recieved:%s\n",buffer );
 		if(buffer[0]=='2')
-		{
+		{printf("TYPE-2 Message Recieved\n");
 			int len=buffer[1]-48;
-			int i=2;
+			int i=3;
 			while(len>0&&isdigit(buffer[i]))
 			{
 				udp_port*=10;
@@ -75,10 +82,54 @@ int main(int argc, char const *argv[])
 	printf("UDP PORT: %d\n",udp_port);
 	printf("Closing existing TCP Connection\n");
 	close(sock);
-	printf("Enter message to send(Maximum 500 characters): ");
+	printf("Enter message to send(Maximum 500 characters):\n");
+	printf("Message Length(>0):");
+	
+	int len2;
+	scanf("%d",&len2);
+	if(len2<=0)
+	{
+		perror("INVALID LENGTH\n");
+			return 0;
+	}
+	
+	printf("\n");
+	//Reading remaining \n
+	char c;
+	scanf("%c",&c);
+	printf("Length: %d\n",len2);
+	printf("Message:");
 	char msg[1000];
-	scanf("%s",msg);
-	printf("Message to send: %s\n",msg);
+	int i=0;
+	msg[0]='3';
+	char temp[4];
+	int len3=len2;
+	while(len2!=0)
+	{
+		temp[i++]=(len2%10+48);
+		len2/=10;
+	}
+	int j=1;
+	i--;
+	while(i>=0)
+	{
+		msg[j++]=temp[i--];
+	}
+	msg[j++]='$';
+	i=j;
+	len2=len3;
+	j=0;
+	while(j<len2)
+	{
+		scanf("%c",msg+i);
+		i++;
+		j++;
+	}
+	msg[i]='\0';
+	printf("Sending TYPE-3 Message:");
+	for(int i=0;msg[i]!='\0';i++)
+		printf("%c",msg[i]);
+	printf("\n");
 	// Creating socket file descriptor for UDP
     if ( (sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
         perror("socket creation failed"); 
@@ -104,14 +155,16 @@ int main(int argc, char const *argv[])
 
     int n,len;
     while(1)
-    {printf("came here\n");
+    {
     	n = recvfrom(sock, (char *)buffer, MAXLINE,  
                 MSG_WAITALL, (struct sockaddr *) &servaddr, 
                 &len); 
     buffer[n] = '\0'; 
     printf("Message Recieved : %s\n", buffer);
-    if(buffer[0]=='4')
+    if(buffer[0]=='4'){
+    	printf("TYPE-4 Message Recieved\n");
     	break;
+    }
     }
 
     close(sock);
--- Assignment-3/server2.c
+++ Assignment-3/server2.c
@@ -18,10 +18,9 @@ int UDP_PORT_INITIAL=8000;
 	
 int main(int argc , char *argv[]) 
 { 
-	//Checking Command line argument
 	if(argc<2)
 	{
-		perror("PORT MISSING\n");
+		perror("PORT Missing\n");
 		return 0;
 	}
 	//Reading PORT
@@ -49,46 +48,49 @@ int main(int argc , char *argv[])
 	int max_sd; 
 	struct sockaddr_in address; 
 		
-	//Data buffer of 1K 
-	char buffer[1025]; 
+	char buffer[1025]; //data buffer of 1K 
 		
 	//set of socket descriptors 
 	fd_set readfds; 
+		
+	//a message 
+	char *message = "ECHO Daemon v1.0 \r\n"; 
 	
-	
-	//initialise all client_socket[] and udp_client_socket[] to 0 so not checked 
+	//initialise all client_socket[] to 0 so not checked 
 	for (i = 0; i < max_clients; i++) 
 	{ 
 		client_socket[i] = 0; 
 		udp_client_socket[i]=0;
 	} 
 		
-	//create a master socket TCP
+	//create a master socket 
 	if( (master_socket = socket(AF_INET , SOCK_STREAM , 0)) == 0) 
 	{ 
-		perror("master socket creation failed"); 
+		perror("socket failed"); 
 		exit(EXIT_FAILURE); 
 	} 
 	
-	//set master socket to allow multiple connections
-	if( setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0 ) 
+	//set master socket to allow multiple connections , 
+	//this is just a good habit, it will work without this 
+	if( setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, 
+		sizeof(opt)) < 0 ) 
 	{ 
 		perror("setsockopt"); 
 		exit(EXIT_FAILURE); 
 	} 
 	
 	//type of socket created 
-	address.sin_family = AF_INET; 	//ipv4
-	address.sin_addr.s_addr = INADDR_ANY; 	//Accept incoming connection on all ips assigned to the machine
-	address.sin_port = htons( PORT ); 		//Custom Port
+	address.sin_family = AF_INET; 
+	address.sin_addr.s_addr = INADDR_ANY; 
+	address.sin_port = htons( PORT ); 
 		
-	//bind the socket to localhost port provided by user
+	//bind the socket to localhost port 8888 
 	if (bind(master_socket, (struct sockaddr *)&address, sizeof(address))<0) 
 	{ 
-		perror("master socket bind failed"); 
+		perror("bind failed"); 
 		exit(EXIT_FAILURE); 
 	} 
-	printf("Master Socket listening on port %d \n", PORT); 
+	printf("Listener on port %d \n", PORT); 
 	printf("Maximum concurrent connections supported: %d\n", max_clients); 
 		
 	//try to specify maximum of 3 pending connections for the master socket 
@@ -118,10 +120,8 @@ int main(int argc , char *argv[])
 			sd = client_socket[i]; 
 				
 			//if valid socket descriptor then add to read list 
-			if(sd > 0) {
+			if(sd > 0) 
 				FD_SET( sd , &readfds);
-				//printf("Adding TCP: %d:%d\n",i,sd);
-			}
 
 			//highest file descriptor number, need it for the select function 
 			if(sd > max_sd) 
@@ -131,10 +131,8 @@ int main(int argc , char *argv[])
 			sd = udp_client_socket[i]; 
 
 			//if valid UDP socket descriptor then add to read list 
-			if(sd > 0){
+			if(sd > 0) 
 				FD_SET( sd , &readfds);
-				//printf("Adding UDP: %d:%d\n",i,sd);
-			}
 				
 			//highest file descriptor number, need it for the select function 
 			if(sd > max_sd) 
@@ -147,13 +145,7 @@ int main(int argc , char *argv[])
 	
 		if ((activity < 0) && (errno!=EINTR)) 
 		{ 
-			printf("select error \n");
-			if(errno==EBADF)
-				printf("1\n"); 
-			else if(errno==EINVAL)
-				printf("2\n");
-			else if(errno==ENOMEM)
-				printf("3\n");
+			printf("select error"); 
 		} 
 			
 		//If something happened on the master socket , 
@@ -168,7 +160,7 @@ int main(int argc , char *argv[])
 			} 
 			
 			//inform user of socket number - used in send and receive commands 
-			printf("New connection , socket fd is %d | ip is : %s | port : %d \n" , new_socket , inet_ntoa(address.sin_addr) , ntohs 
+			printf("New connection , socket fd is %d , ip is : %s , port : %d \n" , new_socket , inet_ntoa(address.sin_addr) , ntohs 
 				(address.sin_port)); 
 				
 			//add new socket to array of sockets 
@@ -199,7 +191,7 @@ int main(int argc , char *argv[])
 					//Somebody disconnected , get his details and print 
 					getpeername(sd , (struct sockaddr*)&address , \ 
 						(socklen_t*)&addrlen); 
-					printf("Host disconnected IP: %s | PORT: %d \n" , 
+					printf("Host disconnected , ip %s , port %d \n" , 
 						inet_ntoa(address.sin_addr) , ntohs(address.sin_port)); 
 						
 					//Close the socket and mark as 0 in list for reuse 
@@ -207,21 +199,21 @@ int main(int argc , char *argv[])
 					client_socket[i] = 0; 
 				} 
 					
-				//Some message has arrived
+				//Echo back the message that came in 
 				else
 				{ 
 					//set the string terminating NULL byte on the end 
 					//of the data read 
-					buffer[valread] = '\0';
-					//UDP PORT Request 
-					if(valread>0&&buffer[0]=='1')	
+					buffer[valread] = '\0'; 
+					if(valread>0&&buffer[0]=='1')	//UDP PORT Request
 					{getpeername(sd , (struct sockaddr*)&address , \ 
 						(socklen_t*)&addrlen); 
-					printf("HOST: IP: %s | PORT: %d asks UDP PORT\n" ,inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
+					printf("Host: ip %s , port %d asks UDP PORT\n" , 
+						inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
 
 						// Creating socket file descriptor for UDP
 					    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
-					        perror("UDP socket creation failed"); 
+					        perror("socket creation failed"); 
 					        exit(EXIT_FAILURE); 
 					    } 
 
@@ -251,11 +243,7 @@ int main(int argc , char *argv[])
 					    } 
 					    buffer[0]='2';
 					    buffer[1]='4';
-					    buffer[2]='$';
 					    int x=port%10;
-					    port/=10;
-					    buffer[6]=x+48;
-					    x=port%10;
 					    port/=10;
 					    buffer[5]=x+48;
 					    x=port%10;
@@ -264,7 +252,10 @@ int main(int argc , char *argv[])
 					    x=port%10;
 					    port/=10;
 					    buffer[3]=x+48;
-					    buffer[7]='\0';
+					    x=port%10;
+					    port/=10;
+					    buffer[2]=x+48;
+					    buffer[6]='\0';
 					    send(sd , buffer , strlen(buffer) , 0 ); 
 					}
 					
@@ -273,12 +264,14 @@ int main(int argc , char *argv[])
 		} 
 	
 		//else its some IO operation on some UDP Socket
-		for (int i2 = 0; i2 < max_clients; i2++) 
+		for (i = 0; i < max_clients; i++) 
 		{ 
-			sd = udp_client_socket[i2]; 
+			sd = udp_client_socket[i]; 
+			//printf("sd: %d\n",sd);
 			
 			if (FD_ISSET( sd , &readfds)) 
-			{ 
+			{ //printf("Came here\n");
+				//Check if it was for closing , and also read the 
 				//incoming message 
 				int len,n;
 				len = sizeof(cliaddr);
@@ -290,39 +283,19 @@ int main(int argc , char *argv[])
 					if(n>0)
 					{
 						
-					printf("Host: IP: %s | PORT: %d | sends message: %s\n" ,inet_ntoa(cliaddr.sin_addr) , ntohs(cliaddr.sin_port),buffer);
-					if(buffer[0]=='3')
-					{
-						int x=1;
-						int len=0;
-						while(buffer[x]!='$')
-						{
-							len*=10;
-							len+=(buffer[x]-48);
-							x++;
-						}
-						char msg[1024];
-						int len2=len;
-						int temp=x+1;
-						int i=0;
-						while(len2>0&&buffer[temp]!='\0')
-						{
-							msg[i++]=buffer[temp++];
-							len2--;
-						}
-						msg[i]='\0';
-						printf("TYPE-3 | MESSAGE LENGTH:%d | MESSAGE:%s\n",len,msg);
-						printf("Sending TYPE-4 message\n");
-						if(!sendto(sd, (const char *)"4\0", 2, MSG_CONFIRM, (const struct sockaddr *) &cliaddr, sizeof(cliaddr))){
+					printf("Host: ip %s , port %d sends message: %s\n" , 
+						inet_ntoa(cliaddr.sin_addr) , ntohs(cliaddr.sin_port),buffer);
+						printf("Sending Type 4 message\n");
+						if(!sendto(sd, (const char *)"4", 1, 
+        			MSG_CONFIRM, (const struct sockaddr *) &cliaddr,  
+            			sizeof(cliaddr))){
             				perror("type 4 message failed"); 
 					        exit(EXIT_FAILURE); 
 						}
 					//Close the socket and mark as 0 in list for reuse 
-					printf("Closing UDP Socket\n");
+						printf("Closing socket\n");
 					close( sd ); 
-					udp_client_socket[i2] = 0; 
-					}
-						
+					udp_client_socket[i] = 0; 
 					}
 					
 				
